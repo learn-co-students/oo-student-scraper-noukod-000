884f5768dec378779541a99f4a3ff32786dd250d
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-24 14:18:42 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,22 @@
 class Student
-
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|att, val| self.send(("#{att}="), val)}
+    @@all << self
   end
 
-  def self.create_from_collection(students_array)
-    
+  def self.create_from_collection(students)
+    students.each { |student_hash| self.new(student_hash) }
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each { |key, val| self.send(("#{key}="), val) }
   end
 
   def self.all
-    
+    @@all
   end
 end
-
