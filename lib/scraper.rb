56495483d70b8b5bfaa705e673ccb2680b504475
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-07 20:26:44 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,15 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
+# This is a class method takes in an argument of a student's profile URL
 class Scraper
 
+  # this method is responsible for scraping the index page that lists all of the students
   def self.scrape_index_page(index_url)
     page = Nokogiri::HTML(open(index_url))
     students = []
-    
+
     page.css("div.student-card").each do |student|
       name = student.css(".student-name").text
       location = student.css(".student-location").text
@@ -17,11 +20,12 @@ class Scraper
                       :profile_url => profile_url
                     }
 
-      students.push(student_info)
+      students << student_info
       end
       students
   end
 
+  # This method is responsible for scraping an individual student's profile page
   def self.scrape_profile_page(profile_url)
     page = Nokogiri::HTML(open(profile_url))
     student = {}
@@ -45,6 +49,4 @@ class Scraper
       student[:bio] = page.css("div.description-holder p").text
       student
     end
-
 end
-
